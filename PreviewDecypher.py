2ee782d158d739b246dc0ef3072d7df3f6a4609b
cwl <cwl@localhost.localdomain>
2015-11-03 12:22:23 -0800

    Added preview problem- Python Decypher

---
+++ PreviewDecypher.py
@@ -0,0 +1,8 @@
+x = raw_input("enter the password: ");
+y = "ko}wmzhugQocQoQhbois";
+for c in x:
+	y += chr(ord(c) ^ 14);
+	if y == "ko}wmzhugQocQoQhbois":
+		print "congratz the flag is " + y;
+	else:
+		print "nope";
